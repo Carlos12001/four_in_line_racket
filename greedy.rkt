7a50a767ee90b05fc3ce7d1722f22f3fe3e41f66
David Robles <47421546+diabloget@users.noreply.github.com>
2023-03-10 07:12:45 -0600

    Se añadió función para procesar puntuaciones
    Se agregaron varias funciones que agregan una puntuación a una coordenada en la matriz, además de una función para balancear esta puntuación en base a si es buena para el AI, buena para el contrincante o buena en la próxima jugada para el contrincante.

--- greedy.rkt
+++ greedy.rkt
@@ -26,8 +26,32 @@
    (list 1 2 1 2 1 2 1 2)
    (list 2 1 2 1 2 1 2 1)))
 
+(define tabla3
+  (list 
+   (list 0 0 0 0 0 0 0 0)
+   (list 0 1 0 0 0 1 0 0)
+   (list 0 0 1 0 1 0 0 0)
+   (list 0 2 1 0 1 2 0 0)
+   (list 0 0 2 1 2 0 0 0)
+   (list 0 0 0 1 0 1 0 0)
+   (list 0 0 0 2 0 0 0 0)
+   (list 0 0 0 0 0 0 0 0)))
+
+(define tabla4
+  (list 
+   (list 0 0 0 0 0 0 0 0)
+   (list 0 0 0 0 0 0 0 0)
+   (list 0 0 2 2 2 0 0 0)
+   (list 0 0 0 0 0 0 0 0)
+   (list 0 1 0 0 0 0 0 0)
+   (list 0 0 0 0 0 0 0 1)
+   (list 1 0 2 2 2 0 0 1)
+   (list 2 1 2 1 2 1 2 1)))
+
+         
 ;Recibe Matriz, valor del jugador a verificar, valor de jugador enemigo, indice de fila, indice columna, contador de columna para moverse horizontalmente, contador de fichas aliadas sucesivas, contador puntos
 (define (ver_izq tablero jugador enem i j cont_h cont_f punt)
+  ;(displayln (list 'ver_izq cont_h cont_f punt))
   (cond
     ((equal? cont_f 3) 1000) ;3 fichas aliadas alrededor, 1000 puntos ya que es gane
     ((or (< cont_h 0) (< cont_h (- j 3)))
@@ -42,6 +66,7 @@
 
 ;Continua el trabajo de la funcion anterior pero hacia lado derecho de la fila
 (define (ver_der tablero jugador enem i j cont_h cont_f punt)
+  ;(displayln (list 'ver_der cont_h cont_f punt))
   (cond
     ((equal? cont_f 3) 1000) ;3 fichas aliadas alrededor, 1000 puntos ya que es gane
     ((or (= cont_h (length (list-ref tablero i))) (> cont_h (+ j 3))) ;verifica no se haya salido del rango a verificar
@@ -102,13 +127,13 @@
 
 ;Recibe Matriz, valor del jugador a verificar, valor de jugador enemigo, indice de fila, indice columna, contador de columna para moverse horizontalmente, contador de fichas aliadas sucesivas, contador puntos
 (define (ver_izqb tablero jugador enem i j cont_h cont_v cont_f punt)
-  ;(displayln (list 'ver_izqa cont_h cont_v cont_f punt))
+  ;(displayln (list 'ver_izqb cont_h cont_v cont_f punt))
   (cond
     ((equal? cont_f 3) 1000) ;3 fichas aliadas alrededor, 1000 puntos ya que es gane
     ((or (< cont_h 0) (< cont_h (- j 3)) (= cont_v (length tablero)) (> cont_v (+ i 3)))
-     (ver_derb tablero jugador enem i j (+ j 1) (+ i 1) cont_f punt)) ;verifica no se haya salido del rango a verificar, pasa a verificar lado opuesto.
+     (ver_dera tablero jugador enem i j (+ j 1) (- i 1) cont_f punt)) ;verifica no se haya salido del rango a verificar, pasa a verificar lado opuesto.
     ((equal? (list-ref (list-ref tablero cont_v) cont_h) enem)
-     (ver_derb tablero jugador enem i j (+ j 1) (+ i 1) cont_f (- punt 10))) ;Si se topa con ficha enemiga, resta puntos y pasa a verificar lado opuesto
+     (ver_dera tablero jugador enem i j (+ j 1) (- i 1) cont_f (- punt 10))) ;Si se topa con ficha enemiga, resta puntos y pasa a verificar lado opuesto
     ((equal? (list-ref (list-ref tablero cont_v) cont_h) 0)
      (ver_izqb tablero jugador enem i j (- cont_h 1) (+ cont_v 1) cont_f (+ punt 5))) ;Topo con espacio vacío, suma puntos pero no muchos.
     (else
@@ -117,7 +142,7 @@
 
 ;Continua el trabajo de la funcion anterior pero hacia lado derecho de la fila
 (define (ver_dera tablero jugador enem i j cont_h cont_v cont_f punt)
-  ;(displayln (list 'ver_derb cont_h cont_v cont_f punt))
+  ;(displayln (list 'ver_dera cont_h cont_v cont_f punt))
   (cond
     ((equal? cont_f 3) 1000) ;3 fichas aliadas alrededor, 1000 puntos ya que es gane
     ((or (= cont_h (length (list-ref tablero i))) (> cont_h (+ j 3)) (< cont_v 0) (< cont_v (- i 3))) ;verifica no se haya salido del rango a verificar
@@ -125,9 +150,9 @@
     ((equal? (list-ref (list-ref tablero cont_v) cont_h) enem) ;Si se topa con ficha enemiga, resta puntos y finaliza
      (- punt 10))
     ((equal? (list-ref (list-ref tablero cont_v) cont_h) 0) ;Valor igual a 0 suma puntos, pero pocos.
-     (ver_derb tablero jugador enem i j (+ cont_h 1) (- cont_v 1) cont_f (+ punt 5)))
+     (ver_dera tablero jugador enem i j (+ cont_h 1) (- cont_v 1) cont_f (+ punt 5)))
     (else
-     (ver_derb tablero jugador enem i j (+ cont_h 1) (- cont_v 1) (+ cont_f 1) (+ punt 10))) ;Ficha aliada encontrada, suma más puntos.
+     (ver_dera tablero jugador enem i j (+ cont_h 1) (- cont_v 1) (+ cont_f 1) (+ punt 10))) ;Ficha aliada encontrada, suma más puntos.
     ))
 
 
@@ -136,7 +161,7 @@
 
 ;Continua el trabajo de la funcion anterior pero hacia lado derecho de la fila
 (define (ver_b tablero jugador enem i j cont_v cont_f punt)
-  ;(displayln (list 'ver_b cont_h cont_v cont_f punt))
+  ;(displayln (list 'ver_b cont_v cont_f punt))
   (cond
     ((equal? cont_f 3) 1000) ;3 fichas aliadas alrededor, 1000 puntos ya que es gane
     ((or (= cont_v (length tablero)) (> cont_v (+ j 3))) ;verifica no se haya salido del rango a verificar
@@ -160,7 +185,7 @@
 
 
 ;-----------------------
-;        GREEDY
+;       El Voraz
 ;-----------------------
 
 ;(define (greedy board player enemy)
@@ -191,8 +216,66 @@
     
 
 
+;-----------------------
+;       Objetivo
+;-----------------------
+
+(define (getij lst index num)
+  (cond
+    ((= num 1)
+     (car (list-ref lst index)))
+    (else
+     (cadr (list-ref lst index)))))
+
+;(calculator tabla3 1 2 '((3 3)) 0) Usado para verificar su funcionamiento
+
+(define (calculator board player enemy fea_res index)
+  (ver_izq board player enemy (getij fea_res index 1) (getij fea_res index 2) (- (getij fea_res index 2) 1) 0
+           (ver_izqa board player enemy (getij fea_res index 1) (getij fea_res index 2) (- (getij fea_res index 2) 1) (- (getij fea_res index 1) 1) 0
+                     (ver_izqb board player enemy (getij fea_res index 1) (getij fea_res index 2) (- (getij fea_res index 2) 1) (+ (getij fea_res index 1) 1) 0
+                               (ver_b board player enemy (getij fea_res index 1) (getij fea_res index 2) (+ (getij fea_res index 1) 1) 0 0)))))
+
+(define (future-calculator board player enemy fea_res index)
+  (future_aux board player enemy (- (getij fea_res index 1) 1) (getij fea_res index 2)))
+
+(define (future_aux board player enemy i j)
+  (cond
+    ((< i 0) 0)
+    (else
+     (ver_izq board player enemy i j (- j 1) 0
+           (ver_izqa board player enemy i j (- j 1) (- i 1) 0
+                     (ver_izqb board player enemy i j (- j 1) (+ i 1) 0
+                               (ver_b board player enemy i j (+ i 1) 0 0)))))))
+
+           
+
+;(objective tabla4 1 2 '((5 0) (4 7) (3 1)))
+
+(define (objective board player enemy fea_res)
+  (obj_aux board player enemy fea_res 0 '()))
+
+(define (obj_aux board player enemy fea_res index res)
+  (displayln (list 'obj_aux fea_res index res))
+  (cond
+    ((= (length res) (length fea_res))
+     res)
+    (else
+     (obj_aux board player enemy fea_res (+ index 1)
+              (append res (list (list (getij fea_res index 1) (getij fea_res index 2)
+                                      (balance
+                                       (calculator board player enemy fea_res index)
+                                       (calculator board enemy player fea_res index)
+                                       (future-calculator board enemy player fea_res index)
+                                       )))))
+     )))
 
 
+(define (balance playerscore enemyscore futurescore)
+  (cond ((> playerscore 900) 1000)
+        ((> enemyscore 900) 500)
+        ((> futurescore 900) -500)
+        (else playerscore)))
+    
 
 
 
